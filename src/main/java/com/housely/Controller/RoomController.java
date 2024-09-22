9308f8b1365b2ac2990026149184994f373a6a55
user-gp1 <narongrit.pa@kkumail.com>
2024-09-22 22:45:12 +0700

    11

---
+++ src/main/java/com/housely/Controller/RoomController.java
@@ -0,0 +1,12 @@
+package com.housely.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class RoomController {
+
+
+
+}
